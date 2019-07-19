c1909104bcd043f2e1e7958a9704f95cb7126c73
expoure <dkramer97@hotmail.com>
2019-07-19 17:31:21 -0300

    add Venda controller, form and routing

--- backend/src/AppBundle/Form/ClienteType.php
+++ backend/src/AppBundle/Form/ClienteType.php
@@ -29,7 +29,7 @@ class ClienteType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => Cliente::class
+            'data_class' => App\AppBundle\Entity\Cliente::class
         ));
     }
 
