eb384d947b7444738a8a39197063bab713c44e35
expoure <dkramer97@hotmail.com>
2019-07-22 12:56:34 -0300

    controllers bug solution

--- frontend/app/controllers/empresas/editar.js
+++ frontend/app/controllers/empresas/editar.js
@@ -1,4 +1,3 @@
-
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
@@ -8,10 +7,8 @@ export default Controller.extend({
     actions: {
         submit() {
             this.get('model').save().then(() => {
-                this.get('notifications').add(`Empresa "${this.get('model').get('nome')}" foi editada.`, 'building');
+                this.get('notifications').add('Empresa foi editada', 'building');
                 this.transitionToRoute('empresas');
-            }).catch((e) => {
-                this.setProperties({ serverErrors: e.errors.children })
             });
         }
     }
